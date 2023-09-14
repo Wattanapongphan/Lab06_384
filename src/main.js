0982a7758f6dea66e02e23387b19a954b2db1ed6
Wattanapongphan <wattanapongphan@gmail.com>
2023-09-14 19:09:59 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
